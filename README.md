249b4aa6aa6fed2a7a56c92f7d5d42da5956e1f7
Lucas <mafsama@Xubuntu.myguest.virtualbox.org>
2026-01-08 19:36:38 +0100

    Add working grid expansion/decrease
    Allow user to choose the amount of pixels
    Implement hover on new pixels

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Create an Etch-a-Sketch sketchpad using JS to manipulate the DOM. The skills lea
     - DOM manipulation: Create, update, delete nodes
     - CSS: Styling attributes by changing their properties defined in a class ".class" or an ID "#ID"
     - JS: eventListeners
+
+Secret to do list:
+    - Current version contains redundant code. I might be able to use functions with callbacks to shorten the code.
+    - Current version lacks comment code.
+    - Current version is not visually pleasing.
--- index.js
+++ index.js
@@ -1,37 +1,88 @@
 let container = document.getElementById("container");
-let resetBtn = document.querySelector(".reset");
+let resetBtn = document.querySelector(".resetBtn");
 let maxColor = 360;
+let pixelBtn = document.querySelector(".pixelBtn");
+let pixelsGrid = 16;
+let clicks = 0;
 
-for (let i = 0; i < 16; i++) {
-    let col = document.createElement("div");
+let col;
+let pixel;
+
+// if (clicks > 0) {
+//     pixelHover();
+// }
+
+pixelBtn.addEventListener("click", () => {
+    pixelsInput = prompt("How many pixels do u want? (100 max)");
+    container.innerHTML = "";
+
+        for (let i = 0; i < pixelsInput; i++) {
+            col = document.createElement("div");
+            container.appendChild(col);
+
+            for (let j = 0; j < pixelsInput; j++) {
+                pixel = document.createElement("div");
+                pixel.classList.add("pixel");
+                pixel.style.backgroundColor = "white";
+                pixel.style.width = 800/pixelsInput + "px";
+                pixel.style.height = 800/pixelsInput + "px";
+                col.appendChild(pixel);
+            }
+        }
+        let pixels = document.getElementsByClassName("pixel");
+        let pixelsArr = Array.from(pixels);
+
+        for (const pixel of pixelsArr) {
+            pixel.addEventListener("mouseover", () => {
+            let randomColorRed = Math.floor(Math.random() * maxColor);
+            let randomColorGreen = Math.floor(Math.random() * maxColor);
+            let randomColorBlue = Math.floor(Math.random() * maxColor);
+            let rgbColor = `${randomColorRed},${randomColorGreen},${randomColorBlue}`;
+
+            pixel.style.backgroundColor = `rgb(${rgbColor})`;
+            console.log("test");
+            })
+        }
+    }
+)
+
+for (let i = 0; i < pixelsGrid; i++) {
+    col = document.createElement("div");
     container.appendChild(col);
 
-    for (let j = 0; j < 16; j++) {
-        let pixel = document.createElement("div");
-        pixel.classList.add("white","pixel");
+    for (let j = 0; j < pixelsGrid; j++) {
+        pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.style.backgroundColor = "white";
+        pixel.style.width = 800/pixelsGrid + "px";
+        pixel.style.height = 800/pixelsGrid + "px"; 
         col.appendChild(pixel);
     }
 }
 
-let pixels = document.getElementsByClassName("white");
+let pixels = document.getElementsByClassName("pixel");
 let pixelsArr = Array.from(pixels);
 
 for (const pixel of pixelsArr) {
     pixel.addEventListener("mouseover", () => {
-        let randomColorRed = Math.floor(Math.random() * maxColor);
-        let randomColorGreen = Math.floor(Math.random() * maxColor);
-        let randomColorBlue = Math.floor(Math.random() * maxColor);
-        let rgbColor = `${randomColorRed},${randomColorGreen},${randomColorBlue}`;
+    let randomColorRed = Math.floor(Math.random() * maxColor);
+    let randomColorGreen = Math.floor(Math.random() * maxColor);
+    let randomColorBlue = Math.floor(Math.random() * maxColor);
+    let rgbColor = `${randomColorRed},${randomColorGreen},${randomColorBlue}`;
 
-        pixel.classList.remove("white");
-        pixel.style.backgroundColor = `rgb(${rgbColor})`;
-
+    pixel.style.backgroundColor = `rgb(${rgbColor})`;
+    console.log("test");
     })
 }
 
 resetBtn.addEventListener("click", () => {
     for (const pixel of pixelsArr) {
-        pixel.classList.add("white");
         pixel.style.backgroundColor = "white";
     }
 })
+
+
+
+
+
+
